96eb39c1c8cec90fb53b8614a161791e691eec83
Steve Morgan <steve@siempresolutions.co.uk>
2025-04-02 16:33:28 +0100

    Build proof of concept working.

--- src/Umbraco.Community.KeyValuesPropertyEditor/Client/src/propertyeditors/key-values-property-editor-ui.element.ts
+++ src/Umbraco.Community.KeyValuesPropertyEditor/Client/src/propertyeditors/key-values-property-editor-ui.element.ts
@@ -1,4 +1,6 @@
 import { LitElement, css, html, customElement, property, state, query, repeat } from "@umbraco-cms/backoffice/external/lit";
+import { UmbControllerHost } from "@umbraco-cms/backoffice/controller-api";
+//import { umbConfirmModal } from '@umbraco-cms/backoffice/modal';
 import { UmbPropertyEditorUiElement } from "@umbraco-cms/backoffice/property-editor";
 import { UmbPropertyValueChangeEvent } from "@umbraco-cms/backoffice/property-editor";
 
@@ -6,20 +8,20 @@ import { UmbPropertyValueChangeEvent } from "@umbraco-cms/backoffice/property-ed
 This is a work in progress
 
 WHAT WORKS NOW
-* Not much - can add items to a list - the save and publish does store to the db and it loads.
+* You can add, delete and update items - the save and publish stores to the db and it loads.
 * I do check for a new property so that it works to add the first item
 
 TODO:
-1) I'm using the this.value directly, I think it should use the _items and just update the value for save and publish / storing the
-data. What I can't work out is how I'd init _items as I'm unclear on how this.value is populated in the first place - I seem to declare it and it's magically assigned (perhaps in UmbPropertyEditorUiElement?)
-2) Render a list of key value pair text fields with a sorter and a delete button (like the multiple text string controls)
-2)a Allow for edits on existing items
-2b) Check the key is unique and not null on add 
-3) Wire everything up.
+1) I can't work out how I'd init _items (tried in the constructor but it's empty there?) as I'm unclear on how this.value is populated in the first place - I seem to declare it and it's magically assigned (perhaps in UmbPropertyEditorUiElement?)
+2) Sorter
+3) Check the key is unique and not null on add (perhaps a nice config option)
 4) Maybe a backend property type convertor. - if we use a key value pair then recheck items are unique - what to do if some junk duplicates are there?
+5) Style and tidy up
+6) Fix the constructor - init the _items?
+7) Use an umbraco confirm dialog - didn't seem to have the host / controller 
 */
 
-// todo - should these be here or inside the custom element> 
+// todo - should these be here or inside the custom element
 type UmbCommunityKeyValue = {
   key: string;
   value: string;
@@ -28,21 +30,13 @@ type ArrayOf<T> = T[];
 
 @customElement('key-values-property-editor-ui')
 export default class UmbCommunityKeyValuesPropertyEditorUIElement extends LitElement implements UmbPropertyEditorUiElement {
+
   @property()
   public value: ArrayOf<UmbCommunityKeyValue> = [];
 
   /* this isn't used at the moment - how to init with the values from value ? */
   @state()
-  private _items: ArrayOf<UmbCommunityKeyValue> = [
-    {
-      key: "test 1",
-      value: "A string value that goes with test 1"
-    },
-    {
-      key: "test 2",
-      value: "A different string"
-    }
-  ];
+  private _items: ArrayOf<UmbCommunityKeyValue> = [];
 
   @query('#key-value-new-key')
   newNameInp!: HTMLInputElement;
@@ -50,43 +44,108 @@ export default class UmbCommunityKeyValuesPropertyEditorUIElement extends LitEle
   @query('#key-value-new-value')
   newValueInp!: HTMLInputElement;
 
+  //#host: UmbControllerHost;
+
+  constructor(value: ArrayOf<UmbCommunityKeyValue> = [], host: UmbControllerHost) {
+    super();
+
+    //this.#host = host;  // host is undefined.
+    console.log(host);
+
+    // this is empty there - so how do I assign this.value to the _items on "init"?
+    this._items = value;
+    console.log(this._items.length);
+  }
+
+
   #onAddRow() {
     const currentInputTyped: UmbCommunityKeyValue = {
       key: this.newNameInp.value,
       value: this.newValueInp.value
     };
 
-    // update _items though this isn't used at the moment
-    // can't push items on this array - not extensible so copy the array and add on
-    this._items = [...this._items, currentInputTyped];
-
     // check the value is an array, the concatenate o/w create an array with this as the first item
-    this.value = Array.isArray(this.value) ? [...this.value, currentInputTyped] : [currentInputTyped];
+    // todo - update to use this._items not value when the items is set on init.
+    this._items = Array.isArray(this.value) ? [...this.value, currentInputTyped] : [currentInputTyped];
 
-    this.#dispatchChangeEvent();
+    this.#updatePropertyEditorValue();
   }
 
-  #dispatchChangeEvent() {
+  #onDelete(index: number) {
+    // todo - can't get the host to use the umbConfirmModal here - what should it be?
+    // await umbConfirmModal(this.#host, {
+    //   headline: `Delete ${this.value || 'item'}`,
+    //   content: 'Are you sure you want to delete this item?',
+    //   color: 'danger',
+    //   confirmLabel: 'Delete',
+    // });
+   
+    // hack use a confirm yes no dialog for now
+    if (confirm("Are you sure you want to delete this item?")) {
+      this._items = [...this._items.slice(0, index), ...this._items.slice(index + 1)];
+      this.#updatePropertyEditorValue();
+    }
+  }
+
+  private _onEditRowValue(e: InputEvent, index: number) {
+    let currentItem = this._items[index];
+
+    const updatedItem: UmbCommunityKeyValue = {
+      key: currentItem.key,
+      value: (e.target as HTMLInputElement).value
+    };
+
+    this._items = [...this._items.slice(0, index), updatedItem, ...this._items.slice(index + 1)];
+
+    this.#updatePropertyEditorValue();
+  }
+
+  #updatePropertyEditorValue() {
+    this.value = this._items;
     this.dispatchEvent(new UmbPropertyValueChangeEvent());
   }
 
   renderTranslationList() {
     // this is a simple example of just writing out the values - to be replaced with the fields below
-    if (this.value?.length) {
+    // todo - remove this hack when when assigning value to _items issue fixed
+    if (this._items.length === 0 && this.value?.length !== 0) {
+      this._items = this.value;
+    }
+
+    if (this._items?.length) {
+      //return html`
+      //      <ul>
+      //          ${this._items.map((translation) =>
+      //  html`<li>${translation.key} ${translation.value}</li>`)}
+      //      </ul>`
       return html`
-            <ul>
-                ${this.value.map((translation) =>
-        html`<li>${translation.key} ${translation.value}</li>`)}
-            </ul>`
+      <ul>
+        ${repeat(this._items, (item) => item.key, (item, index) => html`
+            <li>
+              <input type="text" name="${index}" value="${item.key}" disabled="disabled"></input>
+              <input type="text" name="${index}" value="${item.value}" @input=${(e: InputEvent) => this._onEditRowValue(e, index)}></input>
+              <uui-button
+						    compact
+						    color="danger"
+						    label="remove ${item.key}"
+						    look="outline"
+						    @click=${() => this.#onDelete(index)}>
+						    <uui-icon name="icon-trash"></uui-icon>
+					    </uui-button>
+            </li> `
+      )
+        }
+      </ul>`;
     } else {
       return html`<span>create an item</span>`;
     }
   }
 
   renderTranslationFields() {
-   if (this.value?.length) {
+    // todo - use this._items when init issue fixed
+    if (this.value?.length) {
      return html`
-           ${repeat(this.value, (item) => item.key, (item, index) => {
+           ${repeat(this._items, (item) => item.key, (item, index) => {
              return html`<p>${index}: ${item.key}: ${item.value}</p>`;
      })}
            `;
@@ -121,6 +180,7 @@ export default class UmbCommunityKeyValuesPropertyEditorUIElement extends LitEle
     return html`
         ${this.renderTranslationList()}
         ${this.renderTranslationFields()}
+            <span>Add a new item</span>
             <uui-input
                 id="key-value-new-key"
                 class="element"
@@ -135,7 +195,7 @@ export default class UmbCommunityKeyValuesPropertyEditorUIElement extends LitEle
                 value=""
             >
             </uui-input>
-            <div id="wrapper"> 
+            <div id="wrapper">
                 <uui-button
                     id="add-row-button"
                     class="element"
@@ -143,7 +203,7 @@ export default class UmbCommunityKeyValuesPropertyEditorUIElement extends LitEle
                     label="Add a row"
                     @click=${this.#onAddRow}
                 >
-                    Add a row
+                    Add a key value item
                 </uui-button>
             </div>
         `;
--- src/Umbraco.Community.KeyValuesPropertyEditor/wwwroot/App_Plugins/UmbracoCommunityKeyValuesPropertyEditor/umbraco-community-key-values-property-editor.js
+++ src/Umbraco.Community.KeyValuesPropertyEditor/wwwroot/App_Plugins/UmbracoCommunityKeyValuesPropertyEditor/umbraco-community-key-values-property-editor.js
@@ -3,7 +3,7 @@ const o = [
     name: "Umbraco Community Key Values Property Editor Property Editor",
     alias: "Umbraco.Community.KeyValuesPropertyEditor.PropertyEditor",
     type: "propertyEditorUi",
-    js: () => import("./key-values-property-editor-ui.element-Sjaxx7tQ.js"),
+    js: () => import("./key-values-property-editor-ui.element-BUoawbdC.js"),
     elementName: "ss-translations-property-editor-ui",
     meta: {
       label: "Umbraco Community Key Values",
@@ -11,16 +11,6 @@ const o = [
       group: "common",
       propertyEditorSchemaAlias: "Umbraco.Plain.Json"
     }
-    //meta: {
-    //  label: "Example Dashboard",
-    //  pathname: "example-dashboard"
-    //},
-    //conditions: [
-    //  {
-    //    alias: 'Umb.Condition.SectionAlias',
-    //    match: 'Umb.Section.Content',
-    //  }
-    //],
   }
 ], r = [
   //...entrypoints,
